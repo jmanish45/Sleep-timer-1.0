b2d5ea6caaca9d6b851d860814c9de767c196066
aditya <adityabhise1111@gmail.com>
2025-02-15 15:26:52 +0530

    timer alert solved

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
-      
+  alert("Video paused automatically after 10 seconds!");
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ["content.js"]
