b58d928b70d7fff230c32de9443571dbb1702eba
Manish Jaiswal <jmanish2406@gmail.com>
2025-02-15 15:39:54 +0530

    new commit

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 chrome.action.onClicked.addListener((tab) => {
       
       chrome.scripting.executeScript({
@@ -7,14 +6,4 @@ chrome.action.onClicked.addListener((tab) => {
       });
     
   });
-=======
-chrome.action.onClicked.addListener((tab) => {
-  alert("Video paused automatically after 10 seconds!");
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["content.js"]
-      });
-    
-  });
->>>>>>> b2d5ea6caaca9d6b851d860814c9de767c196066
   
--- content.js
+++ content.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-setTimeout(() => {
-    let video = document.querySelector("video");
-    if (video) {
-      video.pause();
-      alert("Video paused automatically after 10 seconds!");
-    } else {
-      alert("No video found!");
-    }
-  }, 5000);
-=======
 setTimeout(() => {
     let video = document.querySelector("video");
     if (video) {
@@ -18,5 +7,5 @@ setTimeout(() => {
       alert("No video found!");
     }
   }, 10000);
->>>>>>> b2d5ea6caaca9d6b851d860814c9de767c196066
+
   
