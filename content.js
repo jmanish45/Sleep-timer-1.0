6f24d0d4fa7a588e090f6cc87f6842a5fe85eac6
Manish Jaiswal <jmanish2406@gmail.com>
2025-02-15 15:25:05 +0530

    Initial commit

---
+++ content.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    let video = document.querySelector("video");
+    if (video) {
+      video.pause();
+      alert("Video paused automatically after 10 seconds!");
+    } else {
+      alert("No video found!");
+    }
+  }, 5000);
+  
